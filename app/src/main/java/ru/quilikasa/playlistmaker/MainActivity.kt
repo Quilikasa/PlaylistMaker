be7694ae1dcd0f31349533f559b4cd522b600819
sergey.andreev <Andreev.S.Ser@sberbank.ru>
2026-05-16 01:19:40 +0300

    Добавлена обработка нажатий

--- app/src/main/java/ru/quilikasa/playlistmaker/MainActivity.kt
+++ app/src/main/java/ru/quilikasa/playlistmaker/MainActivity.kt
@@ -1,11 +1,35 @@
 package ru.quilikasa.playlistmaker
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnSearch = findViewById<Button>(R.id.btn_search)
+        val btnLibrary = findViewById<Button>(R.id.btn_library)
+        val btnSettings = findViewById<Button>(R.id.btn_settings)
+
+        val clickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(this@MainActivity, "Нажали на кнопку Поиск!", Toast.LENGTH_SHORT).show()
+            }
+        }
+        btnSearch.setOnClickListener(clickListener)
+
+        btnLibrary.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Нажали на кнопку Медиатека!", Toast.LENGTH_SHORT).show()
+        }
+
+        btnSettings.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Нажали на кнопку Настройки!", Toast.LENGTH_SHORT).show()
+        }
     }
 }
