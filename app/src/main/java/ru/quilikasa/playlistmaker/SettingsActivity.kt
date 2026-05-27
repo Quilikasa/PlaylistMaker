554ed50dbe91f9eb206c31420324827b1da2780e
sergey.andreev <Andreev.S.Ser@sberbank.ru>
2026-05-28 00:22:14 +0300

    Релизованы переходы с экрана настроек

--- app/src/main/java/ru/quilikasa/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/ru/quilikasa/playlistmaker/SettingsActivity.kt
@@ -1,8 +1,11 @@
 package ru.quilikasa.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.FrameLayout
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.net.toUri
 
 class SettingsActivity : AppCompatActivity() {
 
@@ -13,6 +16,30 @@ class SettingsActivity : AppCompatActivity() {
         val btnBack = findViewById<ImageView>(R.id.btn_back)
         btnBack.setOnClickListener {
             finish()
+        }
+
+        val btnShare = findViewById<FrameLayout>(R.id.btn_share)
+        btnShare.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.type = "text/plain"
+            intent.putExtra(Intent.EXTRA_TEXT, getString(R.string.share_link))
+            startActivity(intent)
+        }
+
+        val btnSupport = findViewById<FrameLayout>(R.id.btn_support)
+        btnSupport.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.data = "mailto:".toUri()
+            intent.putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.support_email)));
+            intent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.support_subject));
+            intent.putExtra(Intent.EXTRA_TEXT, getString(R.string.support_text));
+            startActivity(intent);
+        }
+
+        val btnEula = findViewById<FrameLayout>(R.id.btn_eula)
+        btnEula.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, getString(R.string.eula_link).toUri())
+            startActivity(intent)
         }
     }
 }
