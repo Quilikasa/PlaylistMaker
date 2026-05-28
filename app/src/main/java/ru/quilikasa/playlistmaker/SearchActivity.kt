e4aae4944edf99fa0decb7e90c70192c2ab3b96e
sergey.andreev <Andreev.S.Ser@sberbank.ru>
2026-05-28 14:37:41 +0300

    Верстка экрана поиска

--- app/src/main/java/ru/quilikasa/playlistmaker/SearchActivity.kt
+++ app/src/main/java/ru/quilikasa/playlistmaker/SearchActivity.kt
@@ -1,6 +1,15 @@
 package ru.quilikasa.playlistmaker
 
+import android.os.Bundle
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 class SearchActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+
+        val editText = findViewById<EditText>(R.id.edit)
+    }
 }
