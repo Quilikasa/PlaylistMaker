4b8f3ceed2b534b26e94c38cfd93aa225d52c29e
sergey.andreev <Andreev.S.Ser@sberbank.ru>
2026-05-28 23:49:13 +0300

    Кнопка очистки поля ввода

--- app/src/main/java/ru/quilikasa/playlistmaker/SearchActivity.kt
+++ app/src/main/java/ru/quilikasa/playlistmaker/SearchActivity.kt
@@ -1,7 +1,11 @@
 package ru.quilikasa.playlistmaker
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
 import android.widget.EditText
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class SearchActivity : AppCompatActivity() {
@@ -10,6 +14,32 @@ class SearchActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
 
+        val btnBack = findViewById<ImageView>(R.id.btn_back)
+        btnBack.setOnClickListener {
+            finish()
+        }
+
+        val btnClear = findViewById<ImageView>(R.id.btn_clear)
         val editText = findViewById<EditText>(R.id.edit)
+
+        btnClear.setOnClickListener {
+            editText.setText("")
+        }
+
+        val textWatcher = object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {}
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s.isNullOrEmpty()) {
+                    btnClear.visibility = View.GONE
+                } else {
+                    btnClear.visibility = View.VISIBLE
+                }
+            }
+
+        }
+        editText.addTextChangedListener(textWatcher)
     }
 }
