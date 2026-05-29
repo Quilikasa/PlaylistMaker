a6339364d224bf3617e5fb00a3dc521fb396b807
sergey.andreev <Andreev.S.Ser@sberbank.ru>
2026-05-29 22:09:50 +0300

    Полноценная поддержка темной темы

--- app/src/main/java/ru/quilikasa/playlistmaker/SearchActivity.kt
+++ app/src/main/java/ru/quilikasa/playlistmaker/SearchActivity.kt
@@ -1,9 +1,11 @@
 package ru.quilikasa.playlistmaker
 
+import android.content.Context
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
@@ -24,6 +26,9 @@ class SearchActivity : AppCompatActivity() {
 
         btnClear.setOnClickListener {
             editText.setText("")
+
+            val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+            inputMethodManager?.hideSoftInputFromWindow(editText.windowToken, 0)
         }
 
         val textWatcher = object : TextWatcher {
