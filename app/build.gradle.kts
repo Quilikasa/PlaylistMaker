e32a1cc6a921379b5d4a8a790e6391bc5e28ed2f
sergey.andreev <111@222.com>
2026-09-04 15:29:15 +0300

    Добавил логирование http-запросов

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,5 +49,6 @@ dependencies {
     annotationProcessor (libs.glide.compiler)
     implementation(libs.retrofit)
     implementation(libs.retrofit.converter.gson)
+    implementation(libs.okhttp.logging.interceptor)
     implementation(libs.gson)
 }
--- app/src/main/java/ru/quilikasa/playlistmaker/SearchActivity.kt
+++ app/src/main/java/ru/quilikasa/playlistmaker/SearchActivity.kt
@@ -18,6 +18,8 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.google.gson.Gson
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -52,8 +54,15 @@ class SearchActivity : AppCompatActivity() {
 
     private var isClickAllowed = true
 
+    private val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor(HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        })
+        .build()
+
     private val retrofit = Retrofit.Builder()
         .baseUrl(ItunesApiService.BASE_URL)
+        .client(okHttpClient)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 
